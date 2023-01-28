7d981d272c75344842d2a9c832ad471f9d9c24db
Fisher Darling <fdarlingco@gmail.com>
2023-01-28 14:31:23 +0100

    Checkpointing with nice printing

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # Ed tu, Brute?
 
-A simple rust program for bruteforcing large ed25519 key pairs in parallel.
+A simple rust program for bruteforcing large ed25519 key pairs in parallel. Supports checkpointing.
 
 Run with
 ```
 cargo run --release
 ```
 
-The largest key found so far is printed in the format `hex-public-key <-> hex-private-key` like so:
+The public keys of recently found keys are printed to the terminal along with the keys/s and 
+total number of keys checked.
 
 ```
-fffff2906ebe5d556124625fadca871e1acfd5d8d732a30255276f7cfce29815 <-> c42ddfbe718dcaab9d0067c52d776c67f5c1231ccdde885ee6f51d42af8fecb5
+bruteforcing with 8 threads
+[2023-01-28 13:33:18] fffe15b7c1a222c2b2c56bf468c0e6fc284f21e9835349902821095e87b823cf
+[2023-01-28 13:33:18] fffebca1e58e7d14543ef8952cccbcb31caf9ed9349147d3a3796f27161f939b
+[2023-01-28 13:33:18] ffffc70d509dbb98affab37f1c303dfa5b3af3b67c117291bab4cd63b803b55f
+[2023-01-28 13:33:18] fffff95243db7df61b61ae00a59b20fb5f3160559101304d99a8e39d5c789a02
+
+⠠ [00:00:06] 384538.65 keys/s, 2,621,400
+  largest: fffff95243db7df61b61ae00a59b20fb5f3160559101304d99a8e39d5c789a02                                                                                           
 ```
 
-Does not support check-pointing, yet.
+Inside the generated `checkpoint.log` file, you'll find a list of found large key pairs.
+
+When the program starts, it loads the checkpoint file and uses the largest key found in
+that file as the starting point for brute forcing. The checkpoint file can be used across runs.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use ed25519_dalek::Keypair;
-use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+use anyhow::Context;
+use ed25519_dalek::{Keypair, PublicKey, SecretKey};
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Write},
+    path::Path,
+    sync::mpsc::{sync_channel, Receiver, SyncSender},
+    time::Duration,
+};
+
+#[allow(clippy::large_enum_variant)]
+enum WorkerMessage {
+    Largest(Keypair),
+    Progress { iteration_delta: usize },
+}
 
 fn main() {
+    if let Err(e) = run_main() {
+        eprintln!("error running edbrute: {e}");
+    }
+}
+
+fn run_main() -> anyhow::Result<()> {
     let num_threads = num_cpus::get();
 
     println!("bruteforcing with {num_threads} threads");
@@ -14,56 +34,87 @@ fn main() {
         to_threads.push(to_thread);
 
         let to_controller = to_controller.clone();
-        std::thread::spawn(|| {
-            run_thread(from_controller, to_controller);
+
+        std::thread::spawn(move || {
+            run_worker(from_controller, to_controller);
         });
+        std::thread::sleep(Duration::from_millis(100));
     }
 
-    run_controller(to_threads, from_threads);
+    run_controller(to_threads, from_threads).context("unable to start controller thread")?;
+
+    Ok(())
 }
 
-fn run_controller(to_threads: Vec<SyncSender<u64>>, from_threads: Receiver<Keypair>) {
-    let mut rng = rand::thread_rng();
-    let mut largest = Keypair::generate(&mut rng);
-    let mut largest_value = u64::from_be_bytes(largest.public.as_bytes()[0..8].try_into().unwrap());
+fn run_controller(
+    to_threads: Vec<SyncSender<u64>>,
+    from_threads: Receiver<WorkerMessage>,
+) -> anyhow::Result<()> {
+    let spinner = setup_spinner();
 
+    let (mut checkpoint_file, saved_largest_keypair) =
+        checkpoint_with_largest_keypair("checkpoint.log")
+            .context("unable to create checkpoint file")?;
+
+    let mut largest_keypair =
+        saved_largest_keypair.unwrap_or_else(|| Keypair::generate(&mut rand::thread_rng()));
+
+    let mut largest_value = public_key_to_u64(&largest_keypair);
     for sender in &to_threads {
         sender.send(largest_value).unwrap();
     }
 
+    let public_pretty = pretty_print_public(&largest_keypair);
+    spinner.set_message(public_pretty);
+
     while let Ok(keypair) = from_threads.recv() {
-        let value = u64::from_be_bytes(keypair.public.as_bytes()[0..8].try_into().unwrap());
+        match keypair {
+            WorkerMessage::Largest(keypair) => {
+                let value = public_key_to_u64(&keypair);
 
-        if value > largest_value {
-            largest_value = value;
+                if value > largest_value {
+                    largest_value = value;
 
-            for sender in &to_threads {
-                sender.send(largest_value).unwrap();
+                    for sender in &to_threads {
+                        sender.send(largest_value).unwrap();
+                    }
+
+                    largest_keypair = keypair;
+
+                    writeln!(checkpoint_file, "{}", serialize_keypair(&largest_keypair))
+                        .context("unable to save keypair to checkpoint file")?;
+                    checkpoint_file.flush()?;
+
+                    let printed_keypair = pretty_print_public(&largest_keypair);
+                    spinner.println(format!(
+                        "[{}] {}",
+                        chrono::Utc::now().format("%Y-%m-%d %H:%M:%S"),
+                        &printed_keypair
+                    ));
+                    spinner.set_message(printed_keypair);
+                }
             }
-
-            largest = keypair;
-            println!(
-                "{} <-> {}",
-                hex::encode(&largest.public.as_bytes()),
-                hex::encode(&largest.secret.as_bytes())
-            );
+            WorkerMessage::Progress { iteration_delta } => {
+                spinner.inc(iteration_delta as u64);
+            }
         }
     }
+
+    Ok(())
 }
 
-fn run_thread(from_controller: Receiver<u64>, to_controller: SyncSender<Keypair>) {
+fn run_worker(from_controller: Receiver<u64>, to_controller: SyncSender<WorkerMessage>) {
     let mut rng = rand::thread_rng();
     let mut largest_value = from_controller.recv().unwrap();
 
+    let iteration_delta = u16::MAX as usize;
     loop {
-        for _ in 0..1024 {
+        for _ in 0..iteration_delta {
             let pair = Keypair::generate(&mut rng);
-            let public_key = &pair.public;
-
-            let value = u64::from_be_bytes(public_key.as_bytes()[0..8].try_into().unwrap());
+            let value = public_key_to_u64(&pair);
 
             if value > largest_value {
-                to_controller.send(pair).unwrap();
+                to_controller.send(WorkerMessage::Largest(pair)).unwrap();
                 largest_value = value;
             }
         }
@@ -71,5 +122,82 @@ fn run_thread(from_controller: Receiver<u64>, to_controller: SyncSender<Keypair>
         if let Ok(largest_found) = from_controller.try_recv() {
             largest_value = largest_found;
         }
+
+        if to_controller
+            .send(WorkerMessage::Progress { iteration_delta })
+            .is_err()
+        {
+            break;
+        }
     }
+}
+
+fn pretty_print_public(keypair: &Keypair) -> String {
+    hex::encode(keypair.public)
+}
+
+fn serialize_keypair(keypair: &Keypair) -> String {
+    format!(
+        "{},{}",
+        hex::encode(keypair.public.as_bytes()),
+        hex::encode(keypair.secret.as_bytes())
+    )
+}
+
+fn public_key_to_u64(keypair: &Keypair) -> u64 {
+    u64::from_be_bytes(keypair.public.as_bytes()[0..8].try_into().unwrap())
+}
+
+fn checkpoint_with_largest_keypair(
+    path: impl AsRef<Path>,
+) -> anyhow::Result<(File, Option<Keypair>)> {
+    let checkpoint_file = std::fs::File::options()
+        .create(true)
+        .read(true)
+        .append(true)
+        .open(path)
+        .context("unable to open checkpoint file")?;
+
+    let reader = BufReader::new(&checkpoint_file);
+
+    let mut keypairs = Vec::new();
+    for line in reader.lines().flatten() {
+        let (public_hex, secret_hex) = line.split_once(',').context("malformed keypair line")?;
+        let (public_bytes, secret_bytes) = (hex::decode(public_hex)?, hex::decode(secret_hex)?);
+        let (public, secret) = (
+            PublicKey::from_bytes(&public_bytes)?,
+            SecretKey::from_bytes(&secret_bytes)?,
+        );
+
+        keypairs.push(Keypair { public, secret })
+    }
+
+    let starting_key = keypairs.into_iter().max_by_key(public_key_to_u64);
+    Ok((checkpoint_file, starting_key))
+}
+
+fn setup_spinner() -> ProgressBar {
+    let spinner = indicatif::ProgressBar::new_spinner();
+
+    spinner.enable_steady_tick(Duration::from_millis(150));
+    spinner.set_style(
+        ProgressStyle::with_template(
+            "\n{spinner} [{elapsed_precise}] {smoothed_per_sec}, {human_pos} total.\n  largest: {msg}",
+        )
+        .unwrap()
+        .with_key(
+            "smoothed_per_sec",
+            |s: &ProgressState, w: &mut dyn std::fmt::Write| match (
+                s.pos(),
+                s.elapsed().as_millis(),
+            ) {
+                (pos, elapsed_ms) if elapsed_ms > 0 => {
+                    write!(w, "{:.2} keys/s", pos as f64 * 1000_f64 / elapsed_ms as f64).unwrap()
+                }
+                _ => write!(w, "-").unwrap(),
+            },
+        ),
+    );
+
+    spinner
 }
